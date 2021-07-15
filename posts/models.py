6a24848b3c3f29f7a3cdbaa17d4ff9e7ab731672
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-07-15 17:24:15 +0300

    Add post model class

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-from django.db.models.fields import CharField
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class Post(models.Model):
-    image = ClaudinaryField('image')
-    name = CharField(max_length=30)
-    user = model
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=30)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image_caption = models.TextField()
