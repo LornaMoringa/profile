f56db5a77dcf2dad659acfef450f51713d26b568
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-07-15 17:14:52 +0300

    Add files

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class Post(models.Model):
+    image = ClaudinaryField('image')
+    name = CharField(max_length=30)
+    user = model
